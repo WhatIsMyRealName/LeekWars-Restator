714635e2f9ab1157f9899be46e7c6fe04255412d
Bux42 <roszwoka2@gmail.com>
2025-11-05 11:36:54 +0100

    eslint rule allow native image

--- app/eslint.config.mjs
+++ app/eslint.config.mjs
@@ -13,6 +13,12 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  // Global rule overrides
+  {
+    rules: {
+      "@next/next/no-img-element": "off",
+    },
+  },
 ]);
 
 export default eslintConfig;
